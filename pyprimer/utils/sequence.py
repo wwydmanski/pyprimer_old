6076399850f87b21fd30914cb15538af7ac5f86f
wwydmanski <wwydmanski@gmail.com>
2020-05-27 11:12:19 +0200

    refactor: extract directory description

--- pyprimer/utils/sequence.py
+++ pyprimer/utils/sequence.py
@@ -8,6 +8,7 @@ from numba import jit
 from enum import Enum
 from .essentials import Essentials
 
+
 class READ_MODES(Enum):
     CSV = 1
     FASTA = 2
@@ -25,161 +26,172 @@ class PCRPrimer(object):
         self.mode = mode
 
     def describe_primers(self,
-                        primers_path,
-                        Na = 50,
-                        K = 0,
-                        Tris = 0,
-                        Mg = 0,
-                        dNTPs = 0,
-                        shift = 0,
-                        nn_table = None,
-                        tmm_table = None,
-                        imm_table = None,
-                        de_table = None,
-                        dnac1 = 25,
-                        dnac2 = 25,
-                        salt_correction = False):
+                         primers_path,
+                         Na=50,
+                         K=0,
+                         Tris=0,
+                         Mg=0,
+                         dNTPs=0,
+                         shift=0,
+                         nn_table=None,
+                         tmm_table=None,
+                         imm_table=None,
+                         de_table=None,
+                         dnac1=25,
+                         dnac2=25,
+                         salt_correction=False):
         """
         Method that reads primers and parse them into dataframe with all the metadata
         """
         if self.mode == READ_MODES.CSV:
-            #TODO for DataFrame with columns "Header" "Sequence"
+            # TODO for DataFrame with columns "Header" "Sequence"
             # primers_df = pd.read_csv(primers_path)
             # self.dataframe = primers_df
-            raise NotImplementedError("This variant of ReadPrimers method is yet not implemented")
+            raise NotImplementedError(
+                "This variant of ReadPrimers method is yet not implemented")
 
         elif self.mode == READ_MODES.FASTA:
-            #TODO for meged fasta file with all primers
-            raise NotImplementedError("This variant of ReadPrimers method is yet not implemented")
+            # TODO for meged fasta file with all primers
+            raise NotImplementedError(
+                "This variant of ReadPrimers method is yet not implemented")
 
         elif self.mode == READ_MODES.DIRECTORY:
-            primers_df = pd.DataFrame(columns=["Origin", "Target", "ID", "Name", "Sequence", "Version", "Type", "Length", "GC(%)", "AT(%)", "Tm"])
-            filelist = os.listdir(primers_path)
-            groups = {}
-            for f in filelist:
-                seriesdict = {"Origin": [],
-                              "Target": [],
-                              "ID": [],
-                              "Name": [],
-                              "Sequence": [],
-                              "Version": [],
-                              "Type": [],
-                              "Length": [],
-                              "GC(%)": [],
-                              "AT(%)": [],
-                              "Tm": []}
-
-                with open(os.path.join(primers_path,f),"r") as fh:
-                    seqlist = fh.readlines()
-
-                seqlist = [item.strip("\n") for item in seqlist]
-                len_seqlist = len(seqlist)
-                header_idx = list(range(0, len_seqlist, 2))
-                seq_idx = list(range(1, len_seqlist, 2))
-                headers = list(map(lambda i: seqlist[i], header_idx))
-                seqs = list(map(lambda i: seqlist[i], seq_idx))
-
-                for i in range(len(headers)):
-                    name_ = headers[i][1:]
-                    origin_, target_, type_ = headers[i].split("|")
-                    sequence_ = seqs[i]
-                    versions = Essentials.get_all_possible_versions(sequence_)
-                    if type(versions) == type(None):
-                        versions = [sequence_]
-                    length_ = len(seqs[i])
-
-                    for version_ in versions:
-                        seriesdict["Origin"].append(origin_[1:])
-                        seriesdict["Target"].append(target_)
-                        seriesdict["ID"].append("{}_{}".format(origin_[1:],target_))
-                        seriesdict["Name"].append(name_)
-                        seriesdict["Sequence"].append(sequence_)
-                        seriesdict["Version"].append(version_)
-
-                        gc_ = Essentials.GCcontent(version_)
-                        seriesdict["GC(%)"].append(gc_)
-                        seriesdict["AT(%)"].append(100 - gc_)
-
-                        tm_ = Essentials.Tm(seq = version_,
-                                            GC = gc_,
-                                            Na = Na,
-                                            K = K,
-                                            Tris = Tris,
-                                            Mg = Mg,
-                                            dNTPs = dNTPs,
-                                            shift = shift,
-                                            nn_table = nn_table,
-                                            tmm_table = tmm_table,
-                                            imm_table = imm_table,
-                                            de_table = de_table,
-                                            dnac1 = dnac1,
-                                            dnac2 = dnac2,
-                                            salt_correction = salt_correction)
-                        seriesdict["Tm"].append(tm_)
-                        seriesdict["Type"].append(type_)
-                        seriesdict["Length"].append(length_)
-                groups[f] = seriesdict
-            for key, item in groups.items():
-                temp_df = pd.DataFrame(item)
-                primers_df = primers_df.append(temp_df)
-            
-            return primers_df
+            return self._describe_dir(primers_path,
+                                      Na=Na,
+                                      K=K,
+                                      Tris=Tris,
+                                      Mg=Mg,
+                                      dNTPs=dNTPs,
+                                      shift=shift,
+                                      nn_table=nn_table,
+                                      tmm_table=tmm_table,
+                                      imm_table=imm_table,
+                                      de_table=de_table,
+                                      dnac1=dnac1,
+                                      dnac2=dnac2,
+                                      salt_correction=salt_correction)
         else:
-            raise ValueError("Unspecified {} mode, use 'csv', 'directory' or 'fasta' instead".format(self.mode))
+            raise ValueError(
+                "Unspecified {} mode, use 'csv', 'directory' or 'fasta' instead".format(self.mode))
+
+    def _describe_dir(self, primers_path, **kwargs):
+        primers_df = pd.DataFrame(columns=[
+                                  "Origin", "Target", "ID", "Name", "Sequence", "Version", "Type", "Length", "GC(%)", "AT(%)", "Tm"])
+        filelist = os.listdir(primers_path)
+        groups = {}
+        for f in filelist:
+            seriesdict = {"Origin": [],
+                          "Target": [],
+                          "ID": [],
+                          "Name": [],
+                          "Sequence": [],
+                          "Version": [],
+                          "Type": [],
+                          "Length": [],
+                          "GC(%)": [],
+                          "AT(%)": [],
+                          "Tm": []}
+
+            headers = []
+            seqs = []
+            with open(os.path.join(primers_path, f), "r") as fh:
+                for line in fh:
+                    if '>' in line:
+                        headers.append(line.strip()[1:])
+                    else:
+                        seqs.append(line.strip())
+
+            for i in range(len(headers)):
+                name_ = headers[i]
+                origin_, target_, type_ = headers[i].split("|")
+                sequence_ = seqs[i]
+                versions = Essentials.get_all_possible_versions(sequence_)
+                length_ = len(seqs[i])
+
+                for version_ in versions:
+                    seriesdict["Origin"].append(origin_)
+                    seriesdict["Target"].append(target_)
+                    seriesdict["ID"].append("{}_{}".format(origin_, target_))
+                    seriesdict["Name"].append(name_)
+                    seriesdict["Sequence"].append(sequence_)
+                    seriesdict["Version"].append(version_)
+
+                    gc_ = Essentials.GCcontent(version_)
+                    seriesdict["GC(%)"].append(gc_)
+                    seriesdict["AT(%)"].append(100 - gc_)
+
+                    tm_ = Essentials.Tm(seq=version_, GC=gc_, **kwargs)
+                    seriesdict["Tm"].append(tm_)
+                    seriesdict["Type"].append(type_)
+                    seriesdict["Length"].append(length_)
+            groups[f] = seriesdict
+        for key, item in groups.items():
+            temp_df = pd.DataFrame(item)
+            primers_df = primers_df.append(temp_df)
+        return primers_df
+
 
 class Sequence(object):
     def __init__(self, mode):
         self.mode = mode
-    
+
     def describe_sequences(self, seqs_path):
         if self.mode == READ_MODES.CSV:
-            #TODO for DataFrame with columns "Header" "Sequence"
+            # TODO for DataFrame with columns "Header" "Sequence"
             # seqs_df = pd.read_csv(self.seqs_path)
             # self.dataframe = seqs_df
-            raise NotImplementedError("This variant of ReadSequences method is yet not implemented")
+            raise NotImplementedError(
+                "This variant of ReadSequences method is yet not implemented")
 
         elif self.mode == READ_MODES.FASTA:
-            seqs_df = pd.DataFrame(columns = ["Header", "Sense Sequence", "Antisense Sequence", "Length", "N(%)"])
+            seqs_df = pd.DataFrame(
+                columns=["Header", "Sense Sequence", "Antisense Sequence", "Length", "N(%)"])
             with open(seqs_path, "r") as fh:
                 fasta = fh.read()
             fastalist = fasta.split("\n>")
-            seriesdict = {"Header": [], "Sense Sequence": [], "Antisense Sequence": [], "Length": [], "N(%)": []}
+            seriesdict = {"Header": [], "Sense Sequence": [],
+                          "Antisense Sequence": [], "Length": [], "N(%)": []}
             idx = 0
             for element in fastalist:
                 element_list = element.split("\n")
-                header = element_list[0].replace(">","")
-                sequence = ("").join(element_list[1:]).replace("-","N").upper()
+                header = element_list[0].replace(">", "")
+                sequence = ("").join(
+                    element_list[1:]).replace("-", "N").upper()
                 del element_list
                 try:
                     antisense_sequence = Essentials.Antisense(sequence[::-1])
                     length_sequence = len(sequence)
-                    n_percent = np.multiply(np.divide(Counter(sequence).pop("N", 0), length_sequence),100)
+                    n_percent = np.multiply(
+                        np.divide(Counter(sequence).pop("N", 0), length_sequence), 100)
                     seriesdict["Header"].append(header)
                     seriesdict["Sense Sequence"].append(sequence)
                     seriesdict["Antisense Sequence"].append(antisense_sequence)
                     seriesdict["Length"].append(length_sequence)
                     seriesdict["N(%)"].append(n_percent)
                 except:
-                    os.makedirs("./logs/",  mode = 755, exist_ok=True)
-                    with open("./Problematic_{}_{}.txt".format(header[-14:],idx),"w") as f:
+                    os.makedirs("./logs/",  mode=755, exist_ok=True)
+                    with open("./Problematic_{}_{}.txt".format(header[-14:], idx), "w") as f:
                         f.write(sequence)
-                    raise KeyError('Unexpected character in {} [index {}]'.format(header, idx))
+                    raise KeyError(
+                        'Unexpected character in {} [index {}]'.format(header, idx))
                 idx += 1
             seqs_df = seqs_df.append(pd.DataFrame(seriesdict))
 
             return seqs_df
 
         elif self.mode == READ_MODES.DIRECTORY:
-            raise NotImplementedError("This variant of ReadSequences method is yet not implemented")
+            raise NotImplementedError(
+                "This variant of ReadSequences method is yet not implemented")
 
         else:
-            raise ValueError("Unspecified {} mode, use 'csv', 'directory' or 'fasta' instead".format(self.mode))
+            raise ValueError(
+                "Unspecified {} mode, use 'csv', 'directory' or 'fasta' instead".format(self.mode))
+
 
 if __name__ == "__main__":
     test_primer = PCRPrimer("directory")
     test_primer.DescribePrimers("./data/primers")
     test_sequence = Sequence("fasta")
     test_sequence.DescribeSequences("./data/merged.fasta")
-    test_primer.dataframe.to_csv("primers_test_df.csv",index = False)
-    test_sequence.dataframe.to_csv("sequence_test_df.csv", index = False)
-
+    test_primer.dataframe.to_csv("primers_test_df.csv", index=False)
+    test_sequence.dataframe.to_csv("sequence_test_df.csv", index=False)
